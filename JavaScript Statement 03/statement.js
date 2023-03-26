dacd42f93c8f0c4bc5bb3a657b1bf623fb73d0f0
AbdurRahman-34 <get.abdurahman@gmail.com>
2023-03-26 19:36:30 +0600

    statement file update

---
+++ JavaScript Statement 03/statement.js
@@ -0,0 +1,33 @@
+// Semicolons  use 1st Waye :::
+
+const a = 20; 
+const b = 20; 
+const c = 20; 
+
+// Semicolons  use 2nd Waye :::
+const ab = 20, ac = 20, ad = 20;
+
+
+
+
+// JavaScript White Space:::::::::::::::
+let person1 = "Hege";
+let person="Hege";
+
+
+
+// JavaScript Line Length and Line Breaks:::::
+document.getElementById("demo").innerHTML 
+="Hello Dolly!"
+
+
+
+// JavaScript Code Blocks {...}
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
+
+
+//   JavaScript Keywords / reserb word :::::
+// JavaScript keywords are reserved words. Reserved words cannot be used as names for variables.
