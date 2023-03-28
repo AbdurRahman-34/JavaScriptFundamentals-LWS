ee24d6362bcc7687a1ad5d9bc7e8cec97afa8e13
AbdurRahman-34 <get.abdurahman@gmail.com>
2023-03-28 16:32:15 +0600

    syntax code update

---
+++ JavaScript Syntax/syntax.js
@@ -0,0 +1,54 @@
+// JavaScript Variables::::::::একটা নাম দিয়ে ডাটা store করার সিস্টেম 
+let a;
+x = 6;
+
+
+
+
+
+
+// arithmetic operators ( + - * / ) to compute values: কুনু কিছু নতুন ভাবে নিয়ন করা  
+(5 + 6) * 10
+
+
+
+
+
+
+
+// JavaScript uses an assignment operator ( = ) to assign values to variables:
+// যার মাধ্যমে একটা ভ্যালু নতুন ভ্যালুতে  replace করা যায় 
+
+    let x, y;
+    x = 5;
+    y = 6;
+
+
+
+// JavaScript Expressions ::::::: যেখানে value, variable, operators একসাথে হয়ে একটা সিঙ্গল value রিটার্ন করবে। 
+
+      5 * 10;
+      "John" + " " + "Doe"
+
+
+
+
+// JavaScript Keywords ::::::::কুনু একটা কাজ করে 
+let x, y;
+x = 5 + 6;
+y = x * 10;
+
+
+var x, y;
+x = 5 + 6;
+y = x * 10;
+
+
+
+//JavaScript Comments : খাতায় রাফ যেভাবে আমরা করি সে রকম। এই কোড গুলু গুপন থাকে 
+// comment 
+
+
+
+
+
